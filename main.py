e8e78ff481d0741f8e02df4a9179b93fa77f8ebf
keepgoingwang <110523642+keepgoingwang@users.noreply.github.com>
2024-01-09 23:28:43 +0800

    新增重连

--- main.py
+++ main.py
@@ -3,25 +3,96 @@
 '''
 import os
 from utils import *
-from bi import *
-import requests
+# from bi import *
+import shutil
+import json
+import pandas as pd
 
-def main():
-	url = r"https://search.jd.com/Search?keyword=%E6%89%8B%E6%9C%BA&enc=utf-8&wq=%E6%89%8B%E6%9C%BA&pvid=8858151673f941e9b1a4d2c7214b2b52&czLogin=1"
+def main(areas):
+	
+	try:
+		with open(r".\tmps\detail_urls.json", 'r') as f:
+			level_two_urls = json.load(f)
+		step1 = False
+	except:
+		step1 =True
+
 	headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+		"User-Agent": "Mozilla/5.0",
+		}
+	if step1:
+		# 获取每个区域对应的所有房源url
+		print("step 1 >>>>>>")
+		home_url = r"https://xz.lianjia.com" # https://xz.lianjia.com/zufang/tongshanqu/pg1
+		level_one_urls = {}
+		for area_name, pages in areas.items():
+			level_one_urls[area_name] = [ str(home_url + str("/zufang/") + str(area_name) + str("/pg") + str(page)) for page in range(1, int(pages+1))]
+			# print(f"{area_name}有{len(level_one_urls[area_name])}页数据")
+
+		# 获取每套房源对应的具体ur
+		level_two_urls = {}
+		for a_name, urls in level_one_urls.items():
+			page = Pages(urls=urls, headers=headers)
+			
+			matchs = {
+				a_name: '//div[@class="content__list--item--main"]/p/a[@class="twoline"]/@href'
+			}
+			
+			detail_pages = page.get_elements_by_xpath(match=matchs)
+			detail_urls = [str(home_url + str(page)) for _,pages in detail_pages.items() for page in pages]
+
+			level_two_urls[a_name] = detail_urls
+			print(f"{a_name}有{len(detail_urls)}条房源数据")
+		
+		# 新建 tmp 文件夹将获取的urls保存至本地；
+		if not os.path.exists(r'.\tmps'):
+			os.mkdir(r'.\tmps')
+		with open(r".\tmps\detail_urls.json", "w", encoding='utf-8') as f:
+			json.dump(level_two_urls, f, default=str)
+
+	# 获取每个区域每套房源具体信息
+	if 1 == 0:
+		print("step 2 >>>>>>")
+		all_infos = {}
+		for a_name, d_urls in level_two_urls.items():
+			pages = Pages(urls=d_urls)
+			d_matchs = {
+				"title": '//div/p[@class="content__title"]/text()',
+				"areas": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[2]/text()',
+				"towards": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[3]/text()',
+				"floor": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[8]/text()',
+				"elevator": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[9]/text()',
+				"electricity": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[14]/text()',
+				"water": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[12]/text()',
+				"gas": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[1]/li[15]/text()',
+				"visit": '//div[@class="content__article fl"]/div[@class="content__article__info"]/ul[2]/li[5]/text()',
+				"price": '//div[@class="table_content"]/ul/li[@class="table_col font_orange"]/text()',
+				"pay_method": '//div[@class="table_content"]/ul/li[@class="table_col font_gray"]/text()',
+				"traffic": '//div[@class="content__article__info4 w1150"]/ul/li/span/text()'
 			}
 
+			detail_infos = pages.get_elements_by_xpath_one(match=d_matchs)
+			
+			all_infos[a_name] = detail_infos
 
-	page = Pages(urls=url)
-
-	matchs = {
-		"year": '//div[@id="J_goodsList"]/ul/li/div/div[@class="p-price"]',
-		"name": '//div[@id="J_goodsList"]/ul/li/div/div[@class="p-stop"]/text()',
-		"actor": '//div[@id="J_goodsList"]/ul/li/div/div[@class="p-name p-name-type-2"]/text()'
-	}
-	data = page.get_elements_by_xpath(match=matchs)
-	print(data)
+		with open(r".\tmps\all_infos.json", "w", encoding='utf-8') as f:
+			json.dump(all_infos, f)
+		# df = pd.DataFrame(columns=["title", "areas", "towards", "floor", "elevator", "electricity", "water", "gas", "visit", "price", "pay_method", "traffic"])
+		# for a_name, infos in all_infos.items():
+		# 	df_info = dic_2_csv(infos)
+		# 	df_info["region"] = a_name
+		# 	df = pd.concat([df, df_info], axis=0)
+		
+		# df.to_csv(r".\results.csv",encoding='utf-8',index=False)
+		
 
 if __name__ == "__main__":
-	main()
+
+	areas = {
+		"yunlongqu": 75,
+		"gulouqu": 79,
+		"quanshanqu": 100,
+		"tongshanqu": 41	
+	}
+	
+	main(areas)
--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@
 '''
 import re
 import os
+import random
+import threading
+import time
 
 import requests
 from lxml import html,etree
-import random
-
+import pandas as pd
 class Pages():
-    def __init__(self, urls, proxies=None):
+    def __init__(self, urls, proxies=None, headers=None):
         if isinstance(urls, list):
             self.urls = urls
         elif isinstance(urls, str):
@@ -22,9 +24,52 @@ class Pages():
         self.proxies = proxies
         self.responses = []
         self.fail_urls = []
-        self._crawl_pages()
+        self.headers = headers
+        self._crawl_pages_one()
+        # self._crawl()
 
-    def _crawl_pages(self):
+        try_time = 0
+        while len(self.fail_urls) != 0:
+            print(f"开启第{try_time+1}次重新访问")
+            self.urls = self.fail_urls
+            self.fail_urls = []
+            self._crawl_pages_one()
+            try_time += 1
+            if try_time > 10:
+                break
+
+        print(f"--获取成功的数据有{len(self.responses)}条--")
+        print(f"--获取失败的数据有{len(self.fail_urls)}条--")
+
+
+    def _crawl(self):
+        # 创建多线程
+        threads = []
+
+        # 遍历所有URL，创建线程用于抓取页面
+        for url in self.urls:
+            crawl_thread = threading.Thread(target=self._crawl_pages_thred, args=(url,))
+            threads.append(crawl_thread)
+
+        # 启动所有线程
+        for thread in threads:
+            thread.start()
+
+        # 等待所有线程完成
+        for thread in threads:
+            thread.join()
+
+
+    def _crawl_pages_thred(self, url):
+        '''多线程'''
+        try:
+            response = self._get_page(url)
+            self.responses.append(response)
+        except requests.RequestException:
+            self.fail_urls.append(url)
+
+    def _crawl_pages_one(self):
+        '''单线程'''
         for url in self.urls:
             try:
                 response = self._get_page(url)
@@ -35,15 +80,21 @@ class Pages():
     def _get_page(self, url):
         proxy = random.choice(self.proxies) if self.proxies else None
         if proxy:
-            response = requests.get(url, proxies={'http': proxy, 'https': proxy})
+            response = requests.get(url, proxies={'http': proxy, 'https': proxy}, headers=self.headers)
         else:
-            response = requests.get(url)
-        print(response.status_code)
+            response = requests.get(url, headers=self.headers)
         response.raise_for_status()
+        print(response.status_code)
+        time.sleep(random.randint(2,5))
         return response
 
+
+
     def get_elements_by_xpath(self, match:str or dict):
-        '''xpath'''
+        '''xpath:
+            一条match语句匹配出的的数据可为一条或多条
+        '''
+
         data_dict = {}
         for url_response in self.responses:
             tree = html.fromstring(url_response.content)
@@ -54,8 +105,34 @@ class Pages():
                     data_dict["elements"] = data_dict.get(elements, []) + elements
             elif isinstance(match, dict):
                 for key, xpath in match.items():
-                    print(xpath)
                     elements = tree.xpath(xpath)
+                    if elements:
+                        data_dict[key] = data_dict.get(key, []) + elements
+                    else:
+                        print(">>>>>>>>>>>>>>>>>>","\n",url_response.text)
+
+        return data_dict
+
+    
+    def get_elements_by_xpath_one(self, match:str or dict):
+        '''xpath:
+            一条match语句匹配出的的数据只能为一条，若匹配到多条数据，转为tuple作为整体
+        '''
+        data_dict = {}
+        for url_response in self.responses:
+            tree = html.fromstring(url_response.content)
+            # tree = etree.HTML(url_response.text)
+            if isinstance(match, str):
+                elements = tree.xpath(match)
+                if len(elements) > 1:
+                    elements = [elements]
+                if elements:
+                    data_dict["elements"] = data_dict.get(elements, []) + elements
+            elif isinstance(match, dict):
+                for key, xpath in match.items():
+                    elements = tree.xpath(xpath)
+                    if len(elements) > 1:
+                        elements = [elements]
                     if elements:
                         data_dict[key] = data_dict.get(key, []) + elements
 
@@ -72,13 +149,13 @@ class Pages():
             if isinstance(match, dict):
                 for key, pattern in match.items():
                     elements = re.findall(pattern, url_response.decode('utf-8'))
-                    if elemenets:
-                        data_dict[key] = data_dict.get(key, []) + elemenets
+                    if elements:
+                        data_dict[key] = data_dict.get(key, []) + elements
 
         return data_dict
 
 
-def save_csv(data, col_names=None, save_path=""):
+def dic_2_csv(data, col_names=None, save_path=None):
     if col_names is None:
         df = pd.DataFrame.from_dict(data)
     else:
@@ -88,10 +165,8 @@ def save_csv(data, col_names=None, save_path=""):
         df = pd.DataFrame(data, columns=col_names)
 
     if save_path:
-        file_path = save_path + "results.csv"
-    else:
-        file_path = "results.csv"
+        file_path = save_path + "results.csv" 
+        df.to_csv(file_path, index=False, encoding="utf-8")
+        print(f"CSV file '{file_path}' has been created.")
 
-    df.to_csv(file_path, index=False, encoding="utf-8")
-    print(f"CSV file '{file_path}' has been created.")
-
+    return df
